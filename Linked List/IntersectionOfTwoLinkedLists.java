21ba94859484dd2348bee741fce5cf8cdb25d590
dharanik378 <dharanik378@gmail.com>
2024-07-21 11:12:28 +0530

    Update IntersectionOfTwoLinkedLists.java

--- Linked List/IntersectionOfTwoLinkedLists.java
+++ Linked List/IntersectionOfTwoLinkedLists.java
@@ -1,3 +1,9 @@
+/*The logic behind this approach is that both pointers will traverse both lists entirely. 
+When they reach the end of one list, they start over from the beginning of the other list. 
+By the time they meet at the intersection, they will have skipped the same number of nodes. 
+This guarantees that they will meet at the intersection point, or both will reach the end (null) 
+at the same time if there is no intersection.*/
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode skipA=headA;
