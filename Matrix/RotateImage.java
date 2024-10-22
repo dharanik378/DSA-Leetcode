56a412bd80e9354dc79193b56dc54836f6625728
dharanik378 <dharanik378@gmail.com>
2024-10-22 11:46:18 +0530

    Create RotateImage.java

---
+++ Matrix/RotateImage.java
@@ -0,0 +1,35 @@
+
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+// APPROACH
+// Find the transpose of the matrix
+// Reverse the rows of the matrix
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+		
+		for(int i=0;i<n;i++){
+			for(int j=i;j<n;j++){
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+		}
+		
+		for(int i=0;i<n;i++){
+			int low =0;
+			int high = n-1;
+			while(low <high){
+				int temp = matrix[i][low];
+				matrix[i][low] = matrix[i][high];
+				matrix[i][high] = temp;
+				low++;
+				high--;
+			}
+		}
+        
+    }
+}
